1dfa29c3d40c55239e2981b6783c8f441c9d379e
Fazzoliie <fazolixd@gmail.com>
2021-04-12 18:39:40 -0300

    versão 1.1

--- Player.gd
+++ Player.gd
@@ -12,10 +12,14 @@ const ACC = 6000 #aceleração
 const DEC = 10000 #desaceleração
 const FORCA_PULO = 6500
 const GRAVIDADE = 12000
+const DANO = 15
 
-var vida = 100
+#Status do jogador
+var vida_max = 300
+var vida = 300
 var defesa = 10
-var dano = 30
+var danoArma = 0
+var pulo_Max = 2
 
 
 func _ready():
@@ -24,8 +28,12 @@ func _ready():
 
 
 func _input(event):
-	if event.is_action_pressed("space"):
+	if is_on_floor() == true:
+		pulo_Max = 2
+	
+	if event.is_action_pressed("space") and pulo_Max > 0:
 		rapidez_y = -FORCA_PULO
+		pulo_Max = pulo_Max - 1
 	pass
 	
 func _process(delta):
@@ -49,9 +57,13 @@ func _process(delta):
 		rapidez_x -= DEC * delta
 	rapidez_x = clamp(rapidez_x, 0, VEL_MAX)
 	
-	rapidez_y += GRAVIDADE * delta
+	if is_on_floor() == false:
+		rapidez_y += GRAVIDADE * delta
+	if is_on_wall() == true:
+		pulo_Max = pulo_Max + 1
+		rapidez_y = (GRAVIDADE / 1.2) * delta 
 	
 	velocidade.x = rapidez_x * delta * direcao
 	velocidade.y = rapidez_y * delta
-	move_and_slide(velocidade)
-
+	
+	velocidade = move_and_slide(velocidade, Vector2.UP)
