54c7721c0df3eb7b103e21681c9b419e4c1c2b4c
Fazzoliie <fazolixd@gmail.com>
2021-04-18 12:47:43 -0300

    v 1.1

--- player/Player.gd
+++ player/Player.gd
@@ -1,20 +1,66 @@
 extends KinematicBody2D
 
-const UP = Vector2(0, -1) 
+signal life_updated(life)
+signal killed()
+
 const GRAVITY = 1000
 const SPEED = 5500
 const JUMP_FORCE = -12000
 
+export (float) var max_life = 250
+
+onready var life = max_life setget _set_life
+onready var invulnerability = $Invunerability
+onready var anim = $Anim
+
 var velocity = Vector2()
+var jump = 0
+var jump_max = 2
 
 func _physics_process(delta):
-
+	anim.play("Idle")
 	velocity.y+= GRAVITY * delta
 	
 	velocity.x = (Input.get_action_strength("d") - Input.get_action_strength("a")) * SPEED * delta
 	
-	if is_on_floor(): #Verifica se o player está em contato com o chão
-		if Input.is_action_pressed("space"):
+	if is_on_floor():
+		jump = jump_max
+	if jump > 0:
+		if Input.is_action_just_pressed("space"):
 			velocity.y = JUMP_FORCE * delta
+			jump -= 1
 	
-	velocity = move_and_slide(velocity, UP)
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+func damage(amount):
+	if invulnerability.is_stopped():
+		invulnerability.start()
+		_set_life(life - amount)
+		#anim.play("blink")
+
+func kill():
+	pass
+	
+func _set_life(value):
+	var prev_life = life
+	life = clamp(value, 0,max_life)
+	if life != prev_life:
+		emit_signal("life_updated", life)
+		if life == 0:
+			kill()
+			emit_signal("killed")
+	
