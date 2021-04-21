e6b634cc0070d25c027df1ee69f3821e6bd1fa97
Fazzoliie <fazolixd@gmail.com>
2021-04-21 01:14:31 -0300

    v1.1.2
    adicionei o roberto, e mais umas coisas nos inimigos e no ataque dele

--- player/Player.gd
+++ player/Player.gd
@@ -7,60 +7,54 @@ const GRAVITY = 1000
 const SPEED = 5500
 const JUMP_FORCE = -12000
 
-export (float) var max_life = 250
-
-onready var life = max_life setget _set_life
 onready var invulnerability = $Invunerability
 onready var anim = $Anim
+onready var sprite = $Sprite
 
+export var def = 10
+export var damage = 0
+export var life = 300
 var velocity = Vector2()
 var jump = 0
 var jump_max = 2
 
 func _physics_process(delta):
-	anim.play("Idle")
+	
+	_move(delta)
+	
+	_jump(delta)
+	
+	_flip()
+	
+	print(life)
+
+func _die():
+	queue_free()
+
+func _move(delta):
 	velocity.y+= GRAVITY * delta
-	
 	velocity.x = (Input.get_action_strength("d") - Input.get_action_strength("a")) * SPEED * delta
-	
+	velocity = move_and_slide(velocity, Vector2.UP)
+
+func _jump(delta):
 	if is_on_floor():
 		jump = jump_max
 	if jump > 0:
 		if Input.is_action_just_pressed("space"):
 			velocity.y = JUMP_FORCE * delta
 			jump -= 1
-	
-	velocity = move_and_slide(velocity, Vector2.UP)
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-func damage(amount):
-	if invulnerability.is_stopped():
-		invulnerability.start()
-		_set_life(life - amount)
-		#anim.play("blink")
 
-func kill():
-	pass
+func _flip():
 	
-func _set_life(value):
-	var prev_life = life
-	life = clamp(value, 0,max_life)
-	if life != prev_life:
-		emit_signal("life_updated", life)
-		if life == 0:
-			kill()
-			emit_signal("killed")
+	if velocity.y < 0:
+		anim.play("Jump")
+	elif velocity.x > 0:
+		anim.play("Walk")
+		sprite.flip_h = true
+	elif velocity.x < 0:
+		anim.play("Walk")
+		sprite.flip_h = false
+	else:
+		anim.play("Idle")
 	
+
